301f4453a0b03d63871539695a673228c42184bc
krzysztoffila <umowy.fila@gmail.com>
2023-02-12 11:04:41 +0100

    add processOrder method

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
-/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
+/* global Handlebars, utils, dataSource */
+// eslint-enable-line no-unused-vars
 
 {
   'use strict';
@@ -84,10 +85,8 @@
     }
     initAccordion() {
       const thisProduct = this;
-      /*find the clickable trigger(the element that should react to clicking)*/
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       /* START: add event listener to clickable trigger .on event click*/
-      clickableTrigger.addEventListener('click', function (event) {
+      thisProduct.accordionTrigger.addEventListener('click', function (event) {
         /*prevent default action for event*/
         event.preventDefault();
         /* find active product (product that has active class)*/
@@ -106,13 +105,11 @@
         event.preventDefault();
         thisProduct.processOrder();
       });
-
       for (let input of thisProduct.formInputs) {
         input.addEventListener('change', function () {
           thisProduct.processOrder();
         });
       }
-
       thisProduct.cartButton.addEventListener('click', function (event) {
         event.preventDefault();
         thisProduct.processOrder();
@@ -120,14 +117,45 @@
     }
     processOrder() {
       const thisProduct = this;
-      console.log('processOrder, ', thisProduct);
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      //console.log('formData', formData);
+      // set price to default price
+      let price = thisProduct.data.price;
+      // for every category (param)...
+      for (let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+        // for every option in this category
+        for (let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(optionId, option);
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if (formData[paramId] && formData[paramId].includes(optionId)) {
+            // check if the option is not default
+            if (option.default === true) {
+              // add option price to price variable
+              price += option.price;
+            }
+          } else {
+            // check if the option is default
+            if (!option.default === true) {
+              // reduce price variable
+              price -= option.price;
+            }
+          }
+          // update calculated price in the HTML
+          thisProduct.priceElem.innerHTML = price;
+        }
+      }
     }
   }
 
   const app = {
     initMenu: function () {
       const thisApp = this;
-      console.log('initData:', thisApp.data);
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
