2172a73d8580a5b78b9e578f254ec6f20bad9987
krzysztoffila <umowy.fila@gmail.com>
2023-02-12 13:46:12 +0100

    cleaning code

--- src/js/script.js
+++ src/js/script.js
@@ -62,7 +62,6 @@
       thisProduct.initOrderForm();
       thisProduct.initAmountWidget();
       thisProduct.processOrder();
-      //console.log('new Product:', thisProduct);
     }
     renderInMenu() {
       const thisProduct = this;
@@ -87,8 +86,6 @@
     }
     initAccordion() {
       const thisProduct = this;
-      /*find the clickable trigger(the element that should react to clicking)*/
-      //const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       /* START: add event listener to clickable trigger .on event click*/
       thisProduct.accordionTrigger.addEventListener('click', function (event) {
         /*prevent default action for event*/
@@ -105,7 +102,6 @@
     }
     initOrderForm() {
       const thisProduct = this;
-      //console.log(thisProduct.initOrderForm)
       thisProduct.form.addEventListener('submit', function (event) {
         event.preventDefault();
         thisProduct.processOrder();
@@ -124,20 +120,16 @@
       const thisProduct = this;
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      //console.log('formData', formData);
       // set price to default price
       let price = thisProduct.data.price;
-
       // for every category (param)...
       for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        //console.log(paramId, param);
         // for every option in this category
         for (let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          //console.log(optionId, option);
           // check if there is param with a name of paramId in formData and if it includes optionId
           if (formData[paramId] && formData[paramId].includes(optionId)) {
             // check if the option is not default
@@ -193,8 +185,7 @@
     setValue(value) {
       const thisWidget = this;
       const newValue = parseInt(value);
-      /*TODO: Add validation*/
-      /* TODO: Add validation */
+      /* validation */
       if (thisWidget.value !== newValue && !isNaN(newValue)) {
         thisWidget.value = newValue;
       }
@@ -219,7 +210,6 @@
   const app = {
     initMenu: function () {
       const thisApp = this;
-      //console.log('thisApp.data:', thisApp.data);
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
