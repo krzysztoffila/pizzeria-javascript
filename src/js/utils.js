d1c8f78e4051ba8923561884700618efd6a13ffd
krzysztoffila <umowy.fila@gmail.com>
2023-05-03 20:50:47 +0200

    Testing

--- src/js/utils.js
+++ src/js/utils.js
@@ -69,7 +69,9 @@ utils.numberToHour = function (number) {
 utils.hourToNumber = function (hour) {
   const parts = hour.split(':');
 
+
   return parseInt(parts[0]) + parseInt(parts[1]) / 60;
+
 };
 
 utils.dateToStr = function (dateObj) {
